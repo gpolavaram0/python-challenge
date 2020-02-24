1962d21df417137c98321744082e6efbbb1a2240
gpolavaram0 <60547556+gpolavaram0@users.noreply.github.com>
2020-02-24 11:31:46 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -27,7 +27,13 @@ for row in voteList:
         index = candidateList[0].index(candidateName)
         candidateList[1][index] += 1
 
+pyPoll_txt = open("PyPoll.txt", 'w')
+
 resultCounter = 0
+
+pyPoll_txt.write("Election Results\n-------------------------\n")
+pyPoll_txt.write(f"Total Votes: {voteCount}")
+pyPoll_txt.write("\n-------------------------")
 print("Election Results\n-------------------------")
 print(f"Total Votes: {voteCount}")
 print("-------------------------")
@@ -35,6 +41,8 @@ print("-------------------------")
 for candidates in candidateList[0]:
 
     print(candidateList[0][resultCounter] + ": " + str(100*candidateList[1][resultCounter]/voteCount) + "%  (" + str(candidateList[1][resultCounter]) +")")
+    pyPoll_txt.write("\n"+ candidateList[0][resultCounter] + ": " + str(100*candidateList[1][resultCounter]/voteCount) + "%  (" + str(candidateList[1][resultCounter]) +")")
+
     resultCounter += 1
 
 winnerNo = max(candidateList[1])
@@ -42,6 +50,13 @@ winderIndex = candidateList[1].index(winnerNo)
 winnerName = candidateList[0][winderIndex]
 
 #print(candidateList)
+
+pyPoll_txt.write("\n-------------------------")
+pyPoll_txt.write("\nWinner: " + str(winnerName))
+pyPoll_txt.write("\n-------------------------")
 print("-------------------------")
 print("Winner: " + str(winnerName))
 print("-------------------------")
+
+
+pyPoll_txt.close()
