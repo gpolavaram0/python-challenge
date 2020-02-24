72f7de1cd07a53794e9d2d25136a3d4723a19cea
gpolavaram0 <60547556+gpolavaram0@users.noreply.github.com>
2020-02-24 02:58:09 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,47 @@
+import os
+import csv
 
+voteFilePath_csv = os.path.join("Instructions","PyPoll", "Resources", "election_data.csv")
+
+voteFile = open(voteFilePath_csv, 'r')
+voteFileReader = csv.reader(voteFile)
+next(voteFileReader)
+voteList = list(voteFileReader)
+voteCount = 0
+candidateList = [[],[]]
+
+for row in voteList:
+    voteCount += 1
+    if candidateList[0].count(row[2]) == 0:
+        candidateName = row[2]
+        candidateList[0].append(candidateName)
+        candidateList[1].append(0)
+        index = candidateList[0].index(candidateName)
+        candidateList[1][index] += 1
+        #print(index)
+        #for candidate in candidateList:
+
+        #    if row[2] == candidate:
+    else:
+        candidateName = row[2]
+        index = candidateList[0].index(candidateName)
+        candidateList[1][index] += 1
+
+resultCounter = 0
+print("Election Results\n-------------------------")
+print(f"Total Votes: {voteCount}")
+print("-------------------------")
+
+for candidates in candidateList[0]:
+
+    print(candidateList[0][resultCounter] + ": " + str(100*candidateList[1][resultCounter]/voteCount) + "%  (" + str(candidateList[1][resultCounter]) +")")
+    resultCounter += 1
+
+winnerNo = max(candidateList[1])
+winderIndex = candidateList[1].index(winnerNo)
+winnerName = candidateList[0][winderIndex]
+
+#print(candidateList)
+print("-------------------------")
+print("Winner: " + str(winnerName))
+print("-------------------------")
